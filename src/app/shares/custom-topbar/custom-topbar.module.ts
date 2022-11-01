c43377ceaa7121c51cd85c79a3a3720588e9661d
Wisner Celucus <wisnercelicus@gmail.com>
2022-11-01 15:27:46 -0400

    up

--- src/app/shares/custom-topbar/custom-topbar.module.ts
+++ src/app/shares/custom-topbar/custom-topbar.module.ts
@@ -4,6 +4,7 @@ import { CustomTopbarComponent } from './custom-topbar.component';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { CdkMenuModule } from '@angular/cdk/menu';
 
+
 @NgModule({
   declarations: [
     CustomTopbarComponent
