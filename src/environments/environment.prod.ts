73546f0a015617f2e7ca71effa2b2c92e9025caa
Wisner Celucus <wisnercelicus@gmail.com>
2023-12-16 17:02:50 -0500

    add ogs, robot.txt and set api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   PROTOCOL_ROOT: 'http://',
-  FRONTEND_BASE_URL: '', //'http://localhost:4002' || http://domainname.ext
-  API_ROOT_URL: '', //'localhost:8000/api' ||      domainname.ext/api
-  API_BASE_URL: '', // 'localhost:8000/api' ||        domainname.ext/api'
-  PUBLIC_BASE_URL: '', //'localhost:8000' ||              domainname.ext'
-  PUBLIC_API_BASE_URL: '', //'localhost:8000/api' ||  domainname.ext/api
-  GOOGLE_MAP_API_KEY: '',
+  FRONTEND_BASE_URL:'http://clientdemo.info', //'http://45.55.105.252:4003', //'http://localhost:4002', //http://45.55.105.252:8001/
+  API_ROOT_URL: 'http://api.clientdemo.info/api', //'45.55.105.252/api', //'localhost:8000/api',         //'45.55.105.252:8001/api'
+  API_BASE_URL: 'http://api.clientdemo.info/api',// '45.55.105.252/api', // 'localhost:8000/api',         //'45.55.105.252:8001/api'
+  PUBLIC_BASE_URL: 'http://api.clientdemo.info',//'45.55.105.252', //'localhost:8000',          //'45.55.105.252:8001'
+  PUBLIC_API_BASE_URL:'http://api.clientdemo.info/api', //'45.55.105.252/api', //'localhost:8000/api',  //45.55.105.252:8001/api
+  GOOGLE_MAP_API_KEY: 'AIzaSyDtoe5t6M0ZRPNMUjQuOPBs45-mxUKcucU',
   COUNTRIES_API_ROOT_URL: 'https://restcountries.com/v3.1',
 };
