8795f5f6399b330deaf0c87cd848a814f68ceee2
Wisner Celucus <wisnercelicus@gmail.com>
2023-12-16 17:47:57 -0500

    force https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  PROTOCOL_ROOT: 'http://',
-  FRONTEND_BASE_URL:'http://clientdemo.info', //'http://45.55.105.252:4003', //'http://localhost:4002', //http://45.55.105.252:8001/
+  PROTOCOL_ROOT: 'https://',
+  FRONTEND_BASE_URL:'https://clientdemo.info', //'http://45.55.105.252:4003', //'http://localhost:4002', //http://45.55.105.252:8001/
   API_ROOT_URL: 'api.clientdemo.info/api', //'45.55.105.252/api', //'localhost:8000/api',         //'45.55.105.252:8001/api'
   API_BASE_URL: 'api.clientdemo.info/api',// '45.55.105.252/api', // 'localhost:8000/api',         //'45.55.105.252:8001/api'
   PUBLIC_BASE_URL: 'api.clientdemo.info',//'45.55.105.252', //'localhost:8000',          //'45.55.105.252:8001'
@@ -9,3 +9,4 @@ export const environment = {
   GOOGLE_MAP_API_KEY: 'AIzaSyDtoe5t6M0ZRPNMUjQuOPBs45-mxUKcucU',
   COUNTRIES_API_ROOT_URL: 'https://restcountries.com/v3.1',
 };
+
