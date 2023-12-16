f54eab0b507a6d1ae0b8ff789baa07efaca51287
Wisner Celucus <wisnercelicus@gmail.com>
2023-12-16 17:32:20 -0500

    fix url link in api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   PROTOCOL_ROOT: 'http://',
   FRONTEND_BASE_URL:'http://clientdemo.info', //'http://45.55.105.252:4003', //'http://localhost:4002', //http://45.55.105.252:8001/
-  API_ROOT_URL: 'http://api.clientdemo.info/api', //'45.55.105.252/api', //'localhost:8000/api',         //'45.55.105.252:8001/api'
-  API_BASE_URL: 'http://api.clientdemo.info/api',// '45.55.105.252/api', // 'localhost:8000/api',         //'45.55.105.252:8001/api'
-  PUBLIC_BASE_URL: 'http://api.clientdemo.info',//'45.55.105.252', //'localhost:8000',          //'45.55.105.252:8001'
-  PUBLIC_API_BASE_URL:'http://api.clientdemo.info/api', //'45.55.105.252/api', //'localhost:8000/api',  //45.55.105.252:8001/api
+  API_ROOT_URL: 'api.clientdemo.info/api', //'45.55.105.252/api', //'localhost:8000/api',         //'45.55.105.252:8001/api'
+  API_BASE_URL: 'api.clientdemo.info/api',// '45.55.105.252/api', // 'localhost:8000/api',         //'45.55.105.252:8001/api'
+  PUBLIC_BASE_URL: 'api.clientdemo.info',//'45.55.105.252', //'localhost:8000',          //'45.55.105.252:8001'
+  PUBLIC_API_BASE_URL:'api.clientdemo.info/api', //'45.55.105.252/api', //'localhost:8000/api',  //45.55.105.252:8001/api
   GOOGLE_MAP_API_KEY: 'AIzaSyDtoe5t6M0ZRPNMUjQuOPBs45-mxUKcucU',
   COUNTRIES_API_ROOT_URL: 'https://restcountries.com/v3.1',
 };
